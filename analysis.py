43facd16f23f5de0c69aaf40c8470867ae458a9f
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2020-10-06 20:55:49 +0000

    pushing analysis, config, and train

--- analysis.py
+++ analysis.py
@@ -15,14 +15,10 @@ import numpy as np
 
 # model
 keypoints = KeypointsGauss(NUM_KEYPOINTS, img_height=IMG_HEIGHT, img_width=IMG_WIDTH)
-#keypoints.load_state_dict(torch.load('checkpoints/dr_braid_varied/model_2_1_5_0.0026437892680103254.pth'))
-keypoints.load_state_dict(torch.load('checkpoints/cable_varied/model_2_1_12_0.0023301696316083607.pth'))
-#keypoints.load_state_dict(torch.load('checkpoints/undo_reid_termGAUSS_KPTS_ONLY/model_2_1_4_0.003552900240372758.pth'))
-
-#keypoints.load_state_dict(torch.load('checkpoints/undo_reid_term/model_2_1_13.pth'))
-#keypoints.load_state_dict(torch.load('checkpoints/undo_reid_term/model_2_1_14.pth'))
-#keypoints.load_state_dict(torch.load('checkpoints/undo_reid_term_braid_GAUSS_KPTS_ONLY/model_2_1_4_0.003553322965173738.pth'))
-#keypoints.load_state_dict(torch.load('checkpoints/undo_reid_term_capsule_GAUSS_KPTS_ONLY/model_2_1_4_0.0032973564452098247.pth'))
+#keypoints.load_state_dict(torch.load('checkpoints/dr_cable_cycles_6400_GAUSS_KPTS_ONLY/model_2_1_10_0.0029300788630979243.pth'))
+#keypoints.load_state_dict(torch.load('checkpoints/dr_cable_cycles_9K_GAUSS_KPTS_ONLY/model_2_1_18_0.003089638756832411.pth'))
+keypoints.load_state_dict(torch.load('checkpoints/dr_cable_cycles_9.5K_GAUSS_KPTS_ONLY/model_2_1_4_0.0031701664664068427.pth'))
+#keypoints.load_state_dict(torch.load('checkpoints/cable_mask_dset_GAUSS_KPTS_ONLY/model_2_1_8_0.0033715498981842476.pth'))
 
 # cuda
 use_cuda = torch.cuda.is_available()
@@ -36,11 +32,10 @@ transform = transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-#image_dir = 'data/global_cable/images'
-#image_dir = 'data/undo_reid_term_braid/test/images'
-#image_dir = 'data/undo_reid_term_capsule/test/images'
-image_dir = 'data/output'
-#image_dir = 'data/real_braid_1'
+#image_dir = 'data/overhead_hairtie_random_resized_larger'
+image_dir = 'data/hairtie_overcrossing_resized'
+#image_dir = 'data/hairtie_overcrossing_resized_masks'
+#image_dir = 'data/overhead_hairtie_resized_masks'
 classes = {0: "Undo", 1:"Reidemeister", 2:"Terminate"}
 for i, f in enumerate(sorted(os.listdir(image_dir))):
     img = cv2.imread(os.path.join(image_dir, f))
